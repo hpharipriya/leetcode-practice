ebf3bb4ce26e8f1ca2f4e49214b24e40fae25d5f
Haripriya <41777071+hpharipriya@users.noreply.github.com>
2025-06-26 21:49:28 +0200

    Create twosum.py

---
+++ twosum.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+       
+        for i in range(len(nums)):
+            compliment = target - nums[i]
+            if compliment in hashmap:
+                return [i, hashmap[compliment]]
+
+            hashmap[nums[i]] = i
